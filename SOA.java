c9fe1c51620a8dce168a8034128a9b2597de45e0
jmontemayorm <j.montemayor.mm@gmail.com>
2018-06-15 16:43:00 +0200

    Setting up git connection

---
+++ SOA.java
@@ -0,0 +1,125 @@
+/* Back-end of Satellite Orbit Analizer: SAO (Main)
+ * Used to test the Satellite Class before the development of a GUI
+ * 
+ * Author: Javier Montemayor
+ * Created: 2018-06-15
+ * Last update: 2018-06-15
+ * 
+ */
+
+// Orekit Libraries
+import org.orekit.frames.*;
+import org.orekit.time.*;
+import org.orekit.data.*;
+import org.orekit.orbits.*;
+import org.orekit.propagation.*;
+import org.orekit.propagation.analytical.*;
+import org.orekit.bodies.*;
+import org.orekit.utils.*;
+import org.orekit.propagation.events.*;
+import org.orekit.propagation.events.handlers.*;
+import org.orekit.propagation.analytical.tle.*;
+
+// Hipparchus Libraries
+import org.hipparchus.geometry.euclidean.threed.*;
+
+// Java Libraries
+import java.io.*;
+import java.util.*;
+
+// SOA Class
+public class SOA {
+  
+  // Main
+  public static void main(String args[]) {
+    try {
+      long endTime, startTime = System.currentTimeMillis();
+      
+      // CONFIGURATION
+      // The following code is required to configure the Orekit library.
+      File orekitData = new File("Libraries/orekit-data"); // The argument indicates the path to the config folder
+      DataProvidersManager manager = DataProvidersManager.getInstance();
+      manager.addProvider(new DirectoryCrawler(orekitData));
+      
+      // Set (and create if needed) the output folders
+      String outPath = "OutputFolder";
+      String sunPath = outPath + "/SunAngles",
+        earthPath = outPath + "/EarthAngles",
+        accessPath = outPath + "/AccessTimes";
+      
+      File sunDir = new File(sunPath),
+        earthDir = new File(earthPath),
+        accessDir = new File(accessPath);
+      
+      if (!sunDir.exists())
+        sunDir.mkdirs(); // Also creates the output folder
+      if (!earthDir.exists())
+        earthDir.mkdir();
+      if (!accessDir.exists())
+        accessDir.mkdir();
+      
+      // GROUND STATION 1
+      System.out.println("Setting up ground station 1..."); // Progress indicator
+      double longitude = Math.toRadians(7.84965); // Freiburg longitude
+      double latitude = Math.toRadians(47.6652); // Freiburg latitude
+      double altitude = 325.036; // [m]??? 0.325036 km
+      GeodeticPoint stationFreiburg = new GeodeticPoint(latitude, longitude, altitude); // Create location point
+      // Topocentric frame for the ground station.
+      Frame earthFrame = FramesFactory.getITRF(IERSConventions.IERS_2010, true);
+      BodyShape earth = new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+                                             Constants.WGS84_EARTH_FLATTENING,
+                                             earthFrame); // Shape of the Earth
+      TopocentricFrame stationFrameFreiburg = new TopocentricFrame(earth, stationFreiburg, "StationFreiburg");
+      
+      // GROUND STATION 2
+      System.out.println("Setting up ground station 2..."); // Progress indicator
+      longitude = Math.toRadians(-7.84965); // 
+      latitude = Math.toRadians(-47.6652); // 
+      altitude = 325.036; // [m]
+      GeodeticPoint stationUnknown = new GeodeticPoint(latitude, longitude, altitude); // Create location point
+      // Topocentric frame for the ground station.
+      TopocentricFrame stationFrameUnknown = new TopocentricFrame(earth, stationUnknown, "StationUnknown");
+      
+      File sta1Dir = new File(accessPath + "/" + stationFrameFreiburg.getName()),
+        sta2Dir = new File(accessPath + "/" + stationFrameUnknown.getName());
+      
+      if (!sta1Dir.exists())
+        sta1Dir.mkdir();
+      if (!sta2Dir.exists())
+        sta2Dir.mkdir();
+      
+      Satellite sat1 = new Satellite(),
+        sat2 = new Satellite();
+      
+      // Add propagators (random satellites for testing)
+      sat1.setTLEPropagator("1 37846U 11060A   18165.13732692 -.00000060  00000-0  00000-0 0  9997",
+                            "2 37846  56.1041  61.6028 0004426 354.3829   5.6503  1.70475882 41387");
+      sat2.setTLEPropagator("1 33312U 08040A   18155.81913053  .00000031  00000-0  10873-4 0  9991",
+                            "2 33312  97.7864 231.7937 0011171 286.7797  73.2178 14.79883098527520");
+      
+      // Add event detectors
+      double maxCheck  = 60.0;
+      double threshold =  0.001;
+      double elevationDeg = 10.0; // [deg]
+      sat1.setElevationDetector(stationFrameFreiburg, maxCheck, threshold, elevationDeg);
+      sat1.setElevationDetector(stationFrameUnknown, maxCheck, threshold, elevationDeg);
+      sat2.setElevationDetector(stationFrameFreiburg, maxCheck, threshold, elevationDeg);
+      sat2.setElevationDetector(stationFrameUnknown, maxCheck, threshold, elevationDeg);
+      
+      Thread thread_1 = new Thread(sat1),
+        thread_2 = new Thread(sat2);
+      
+      thread_1.start();
+      thread_2.start();
+      
+      thread_1.join();
+      thread_2.join();
+      
+      endTime = System.currentTimeMillis();
+      System.out.println("Done.");
+      System.out.println("Elapsed time: " + (endTime - startTime)/1000.0 + " seconds.");
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
---
+++ Satellite.java
@@ -0,0 +1,293 @@
+/* Back-end of Satellite Orbit Analizer: Satellite Class
+ * 
+ * Author: Javier Montemayor
+ * Created: 2018-06-13
+ * Last update: 2018-06-15
+ * 
+ * To-do:
+ * - Set PrintWriters (Dictionary, HashMap) for AccessTimes
+ * 
+ * To-do in another file (main):
+ * - Create the Earth Stations (or geographic zones? <-- add geographic zone detector)
+ * - Read file with TLEs, set a list of Satellite objects and create all the propagators, add all the detectors
+ * - ...
+ */
+
+// Orekit Libraries
+import org.orekit.frames.*;
+import org.orekit.time.*;
+import org.orekit.data.*;
+import org.orekit.orbits.*;
+import org.orekit.propagation.*;
+import org.orekit.propagation.analytical.*;
+import org.orekit.bodies.*;
+import org.orekit.utils.*;
+import org.orekit.propagation.events.*;
+import org.orekit.propagation.events.handlers.*;
+import org.orekit.propagation.analytical.tle.*;
+
+// Hipparchus Libraries
+import org.hipparchus.geometry.euclidean.threed.*;
+
+// Java Libraries
+import java.io.*;
+import java.util.*;
+
+// Satellite Class
+public class Satellite implements Runnable {
+  // Defaults and Other Finals
+  private static final Locale loc = new Locale("EN","US");
+  
+  // Class Variables
+  private AbsoluteDate initialDate, finalDate, accessBegin, accessEnd;
+  private double duration, stepT;
+  private Propagator propagator;
+  private int accessNum = 1; // Reset before every propagation
+  private PrintWriter sunAnglesPrinter, earthAnglesPrinter;// log???
+  private String sunAnglesName, earthAnglesName, accessTimesName;
+  private static DataProvidersManager manager;
+  private CelestialBody sunBody, earthBody;
+  private Frame inertialFrame;
+  private boolean printSun = false, printEarth = false, printAccess = true;
+  private Map<String,PrintWriter> accessTimesPrinters = new HashMap<String,PrintWriter>();
+  
+  // Satellite Constructor
+  public Satellite() {
+    try {
+      // Try default configuration
+      File orekitData = new File("Libraries/orekit-data");
+      manager = DataProvidersManager.getInstance();
+      manager.addProvider(new DirectoryCrawler(orekitData));
+      
+      // Set celestial bodies
+      sunBody = CelestialBodyFactory.getSun();
+      earthBody = CelestialBodyFactory.getEarth();
+      
+      // Set inertial frame
+      inertialFrame = FramesFactory.getEME2000();
+      
+      ////////////////////////////////////// TEMPORARY CODE FOR TESTING ///////////////////////////////////////////////
+      initialDate = new AbsoluteDate(2021, 01, 01, 00, 00, 00.000, TimeScalesFactory.getUTC());
+      duration = 30.0*24.0*60.0*60.0; // [s]
+      finalDate = initialDate.shiftedBy(duration);
+      stepT = 60.0;
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    } catch (Exception e) {
+      System.out.println("Error on Satellite constructor: " + e);
+    }
+  }
+  
+  // Turn on Sun Angles printer
+  public void turnOnSunAnglesPrinter() {
+    printSun = true;
+  }
+  
+  // Turn off Sun Angles printer
+  public void turnOffSunAnglesPrinter() {
+    printSun = false;
+  }
+  
+  // Turn on Earth Angles printer
+  public void turnOnEarthAnglesPrinter() {
+    printEarth = true;
+  }
+  
+  // Turn off Earth Angles printer
+  public void turnOffEarthAnglesPrinter() {
+    printEarth = false;
+  }
+  
+  // Sets a TLE propagator
+  public boolean setTLEPropagator(String line1, String line2) {
+    try {
+      // Set propagator (if there was a previous one, it will be replaced)
+      TLE tle = new TLE(line1,line2);
+      propagator = TLEPropagator.selectExtrapolator(tle);
+      
+      propagator.setSlaveMode();
+      
+      // Success, return true
+      return true;
+    } catch (Exception e) {
+      System.out.println("Error setting TLE: " + e);
+    }
+    
+    // An error occurred, return false
+    return false;
+  }
+  
+  // Sets an elevation detector for the given station
+  public boolean setElevationDetector(TopocentricFrame station, double maxCheck, double threshold, double elevationDeg) {
+    // If no propagator has been provided, return false
+    if (propagator == null)
+      return false;
+    
+    try {
+      ElevationDetector elevDetect =
+        new ElevationDetector(maxCheck, threshold, station
+                             ).withConstantElevation(Math.toRadians(elevationDeg)
+                                                    ).withHandler(new VisibilityHandler());
+      propagator.addEventDetector(elevDetect);
+      // Success, return true
+      return true;
+    } catch (Exception e) {
+      System.out.println("Error setting elevation detector: " + e);
+    }
+    
+    // An error occurred, return false
+    return false;
+  }
+  
+  // Starts propagation, returns false if something has not been setup // CHANGE NAME TO RUN()?
+  public void run() {
+    // Check if everything is set to perform propagation /////////////////////////////////////////////////////////////////////////// CORRECT FOR WHEN PRINTER INACTIVE DOESNT MATTER
+    if (propagator == null || initialDate == null || finalDate == null || stepT == 0) {
+      // Something is missing (log error?), return false
+      System.out.println("The satellite has not been fully set.");
+      //return false;
+    } else {
+      // All should be set to perform the propagation
+      try {
+        // Reset access counter, set accessBegin as initialDate in case propagation starts during an access
+        accessNum = 1; // <-- Needs one variable per detector, dictionary
+        accessBegin = initialDate;
+        
+        // Set PrintWriters
+        if (printSun)
+          sunAnglesPrinter = new PrintWriter(sunAnglesName,"UTF-8");
+        if (printEarth)
+          earthAnglesPrinter = new PrintWriter(earthAnglesName,"UTF-8");
+        
+        for (AbsoluteDate extrapDate = initialDate; extrapDate.compareTo(finalDate) <= 0; extrapDate = extrapDate.shiftedBy(stepT)) {
+          try {
+            // Get current state
+            SpacecraftState currentState = propagator.propagate(extrapDate);
+            
+//            // Get the absolute date, break down into date and time
+//            AbsoluteDate absDate = currentState.getDate();
+//            DateTimeComponents dateTimeComps = absDate.getComponents(0); // Synched with UTC
+//            DateComponents dateComps = dateTimeComps.getDate();
+//            TimeComponents timeComps = dateTimeComps.getTime();
+//            
+//            // Get month string
+//            String monat = dateComps.getMonthEnum().getCapitalizedAbbreviation();
+//            
+//            // Get the satellite reference frame
+//            LocalOrbitalFrame locFrame =
+//              new LocalOrbitalFrame(inertialFrame,LOFType.VVLH,
+//                                    currentState.getPVCoordinates().toTaylorProvider(inertialFrame),"Satellite Frame");
+//            
+//            // If active, print Solar Angles
+//            if (printSun) {
+//              // Get the respective coordinates
+//              Vector3D sunFromEarth = sunBody.getPVCoordinates(absDate, inertialFrame).getPosition();
+//              Vector3D satFromEarth = currentState.getPVCoordinates().getPosition();
+//              Vector3D sunFromSat = sunBody.getPVCoordinates(absDate, locFrame).getPosition();
+//              
+//              // Get solar angles
+//              double subsol, sunAzim, sunElev;
+//              subsol= Math.toDegrees(Vector3D.angle(sunFromEarth,satFromEarth)); // [deg]
+//              sunAzim = Math.toDegrees(Vector3D.angle(Vector3D.PLUS_I,sunFromSat.add(new Vector3D(0.0,0.0,-sunFromSat.getZ())))); // [deg]
+//              sunElev = Math.toDegrees(Vector3D.angle(sunFromSat,new Vector3D(sunFromSat.getX(),sunFromSat.getY(),0.0))); // [deg]
+//              // Fix sign for sunAzim
+//              if (sunFromSat.getY() < 0) {
+//                sunAzim = 360 - sunAzim;
+//              }
+//              // Fix sign for sunElev
+//              if (sunFromSat.getZ() < 0) {
+//                sunElev = - sunElev;
+//              }
+//              
+//              // Write to solar angles file
+//              sunAnglesPrinter.printf(loc,"%d %s %d %02d:%02d:%06.3f,%07.3f,%07.3f,%07.3f\n",dateComps.getDay(),monat,
+//                                      dateComps.getYear(),timeComps.getHour(),timeComps.getMinute(),timeComps.getSecond(),
+//                                      sunAzim,sunElev,subsol);
+//            }
+//            
+//            // If active, print Earth Angles
+//            if (printEarth) {
+//              // Get the respective coordinates
+//              Vector3D earthFromSat = earthBody.getPVCoordinates(absDate, locFrame).getPosition();
+//              
+//              // Get Earth angles
+//              double earthAzim, earthElev;
+//              if (earthFromSat.getX() == 0 & earthFromSat.getY() == 0) { // Prevents crashing when X and Y are both 0
+//                earthAzim = 0.0; // [deg]
+//                if (earthFromSat.getZ() > 0)
+//                  earthElev = 90.0; // [deg]
+//                else
+//                  earthElev = -90.0; // [deg]
+//              } else {
+//                earthAzim = Math.toDegrees(Vector3D.angle(Vector3D.PLUS_I,earthFromSat.add(new Vector3D(0.0,0.0,-earthFromSat.getZ())))); // [deg]
+//                earthElev = Math.toDegrees(Vector3D.angle(earthFromSat,new Vector3D(earthFromSat.getX(),earthFromSat.getY(),0.0))); // [deg]
+//              }
+//              // Fix sign for earthAzim
+//              if (earthFromSat.getY() < 0) {
+//                earthAzim = 360 - earthAzim;
+//              }
+//              // Fix sign for sunElev
+//              if (earthFromSat.getZ() < 0) {
+//                earthElev = - earthElev;
+//              }
+//              
+//              // Write to Earth angles file
+//              earthAnglesPrinter.printf(loc,"%d %s %d %02d:%02d:%06.3f,%07.3f,%07.3f\n",dateComps.getDay(),monat,
+//                                        dateComps.getYear(),timeComps.getHour(),timeComps.getMinute(),timeComps.getSecond(),
+//                                        earthAzim,earthElev);
+//            }
+          } catch (Exception e) {
+            System.out.println("Error inside the loop: " + e);
+          }
+        }
+        
+        // Close PrintWriters
+        if (printSun)
+          sunAnglesPrinter.close();
+        if (printEarth)
+          earthAnglesPrinter.close();
+        
+      }catch (Exception e) {
+        System.out.println("Error while running: " + e);
+      }
+      System.out.println("Finished propagation.");
+      //return true;
+    }
+  }
+  
+  // Visibility Hanlder for Elevation Detectors
+  private class VisibilityHandler implements EventHandler<ElevationDetector> {
+    public Action eventOccurred(final SpacecraftState s, final ElevationDetector detector,
+                                final boolean increasing) {
+      if (s.getDate().compareTo(initialDate) <= 0)
+        return Action.STOP;
+      if (increasing) {
+        accessBegin = s.getDate();
+        
+        return Action.CONTINUE;
+      } else {
+        accessEnd = s.getDate();
+        
+        try{
+          double visDuration = accessEnd.durationFrom(accessBegin);
+          DateTimeComponents compsBegin = accessBegin.getComponents(0); // Synched with UTC
+          DateTimeComponents compsEnd = accessEnd.getComponents(0); // Synched with UTC
+          DateComponents dateBegin = compsBegin.getDate();
+          DateComponents dateEnd = compsEnd.getDate();
+          TimeComponents timeBegin = compsBegin.getTime();
+          TimeComponents timeEnd = compsEnd.getTime();
+          
+          //accessTimesPrinters.get(detector.getTopocentricFrame().getName()).
+          System.out.printf(loc,"%d,%d %s %d %02d:%02d:%06.3f,%d %s %d %02d:%02d:%06.3f,%07.3f\n",accessNum++,
+                            dateBegin.getDay(),dateBegin.getMonthEnum().getCapitalizedAbbreviation(),dateBegin.getYear(),
+                            timeBegin.getHour(),timeBegin.getMinute(),timeBegin.getSecond(),dateEnd.getDay(),
+                            dateEnd.getMonthEnum().getCapitalizedAbbreviation(),dateEnd.getYear(),timeEnd.getHour(),
+                            timeEnd.getMinute(),timeEnd.getSecond(),visDuration);
+        } catch (Exception ex) {
+          System.out.println("Error calculating or writing to Access Times file: " + ex);
+        }
+        
+        return Action.STOP;
+      }
+    }
+  }
+}
